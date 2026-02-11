4193c370325658ea446800d6772e57860ed095d0
Om Prakash <omprakash.snps@gmail.com>
2026-02-12 00:13:58 +0530

    Initial commit

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+function connectDB() {
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Server is Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+        process.exit(1); // Exit the process with an error code
+    })
+}
+
+module.exports = connectDB;
