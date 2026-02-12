de3865392b16b33c213b6344010b825b0f5b6a8e
Om Prakash <omprakash.snps@gmail.com>
2026-02-13 00:58:56 +0530

    Add nodemailer email service and send welcome email
    Introduce an email service using nodemailer (OAuth2) and send a registration welcome email after user signup. Adds nodemailer to package.json and updates package-lock. Implements src/services/email.service.js (transporter setup, sendEmail helper, sendRegistrationEmail) and calls sendRegistrationEmail from auth controller after successful registration. Also updates .gitignore to ignore editor, build, and log files. Environment variables required: EMAIL_USER, CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
+const emailService = require('../services/email.service');
 
 /**
  * 
@@ -35,6 +36,8 @@ async function userRegisterController(req, res) {
         },
         token
     });
+
+    await emailService.sendRegistrationEmail(user.email, user.name);
 }
 
 async function userLoginController(req, res) {
